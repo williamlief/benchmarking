f95801857c375bbd209b72bf5eeeae695317cd89
Lief <williamlief@gmail.com>
2021-09-25 10:46:15 -0700

    compare_plot created

---
+++ R/compare_plot.R
@@ -0,0 +1,27 @@
+library(tidyverse)
+
+compare_plot <- function(comparison, title) {
+  
+  files <- list.files("output/", comparison, full.names = T)
+  names(files) <- gsub("\\.RDS", "", gsub(".*-", "", files))
+  
+  dat <- map_dfr(files,
+                 readRDS,
+                 .id = "system") %>% 
+    mutate(time = time / 1e9)
+  
+  ggplot(dat, aes(x = expr, y = time, color = system)) +
+    geom_boxplot() +
+    theme_minimal() +
+    labs(y = "time (seconds)", 
+         x = NULL, 
+         subtitle = "Results from Ten Trials, same hardware dual booted",
+         title = title) 
+  
+  ggsave(paste0("figures/", comparison, ".jpg"),
+         bg = "white",
+         width = 7, height = 7)
+}
+
+compare_plot("synth_methods", "Synthetic Control Packages")
+  
